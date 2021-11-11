92bd4911dc2fd98fe1d13e712bd19eeda2dd2873
julianrose27 <94125597+julianrose27@users.noreply.github.com>
2021-11-11 09:23:20 -0600

    Initial commit

---
+++ Cipher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace SubstitutionCipher
+{
+    public class Cipher
+    {
+
+        private string Alphabet = "abcdefghijklmnopqrstuvwx";
+        private Char[] Substitute;
+
+        public Cipher()
+        {
+            this.Substitute = GenerateSubstitute();
+        }
+
+        public Cipher(string Substitute)
+        {
+            this.Substitute = Substitute.ToCharArray();
+        }
+
+        private Char[] GenerateSubstitute()
+        {
+            Char[] Temp = Alphabet.ToCharArray();
+            Random rng = new Random();
+            int i = Temp.Length;
+            while(i > 1)
+            {
+                i--;
+                int r = rng.Next(i + 1);
+                Char value = Temp[r];
+                Temp[r] = Temp[i];
+                Temp[i] = value;
+            }
+            Console.WriteLine(Temp);
+            return Temp;
+        }
+
+        public string Encrypt(string Message)
+        {
+            Char[] m = Message.ToCharArray();
+            string EncryptedMessage = "";
+
+            foreach(Char c in m)
+            {
+                EncryptedMessage += Substitute[Array.IndexOf(Alphabet.ToCharArray(), c)];
+            }
+            return EncryptedMessage;
+        }
+
+        public string Decrypt(string Message)
+        {
+            Char[] m = Message.ToCharArray();
+            string DecryptedMessage = "";
+
+            foreach(Char c in m)
+            {
+                DecryptedMessage += Alphabet[Array.IndexOf(Substitute, c)];
+            }
+            return DecryptedMessage;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SubstitutionCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cipher c = new Cipher();
+            string Message = "hello";
+            string EncryptedMessage = c.Encrypt(Message);
+            Console.WriteLine($"Encrypted Message: {EncryptedMessage}");
+            Console.WriteLine($"Decryped Message: {c.Decrypt(EncryptedMessage)}");
+        }
+    }
+}
